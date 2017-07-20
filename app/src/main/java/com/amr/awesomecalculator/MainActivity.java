a81c81782fc063bf38fac0acab70495c51e13cae
Hassan Hamdy <hassanhamdyriad@yahoo.com>
2017-07-20 17:18:45 +0200

    recyclerView

--- app/src/main/java/com/amr/awesomecalculator/MainActivity.java
+++ app/src/main/java/com/amr/awesomecalculator/MainActivity.java
@@ -2,55 +2,37 @@ package com.amr.awesomecalculator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView rvCalcButtons;
+    private ArrayList<String> Data;
+    private Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        String[] text = new String[]{
+                "7", "8", "9", "Del", "this", "4", "5", "6", "*", "/",
+                "1", "2", "3", "+", "-", "=", "0", ".", "%", "e",
+                "(", ")", "ln", "pi", "7", "7", "7", "7", "7", "7",
+                "7", "7", "7", "7", "7"
+        };
+
         rvCalcButtons = (RecyclerView) findViewById(R.id.rvCalcButtons);
-        rvCalcButtons.addItemDecoration(new MarginDecoration(this));
-        rvCalcButtons.setHasFixedSize(true);
-
-        final HeaderNumberedAdapter adapter = new HeaderNumberedAdapter(35);
+        Data = new ArrayList<>(Arrays.asList(text));
+        adapter = new Adapter(getApplicationContext(), Data);
         rvCalcButtons.setAdapter(adapter);
+        rvCalcButtons.setLayoutManager(new GridLayoutManager(getApplicationContext(), 5));
 
-        SpannedGridLayoutManager manager2 = new SpannedGridLayoutManager(
-                new SpannedGridLayoutManager.GridSpanLookup() {
-                    @Override
-                    public SpannedGridLayoutManager.SpanInfo getSpanInfo(int position) {
-                        if (adapter.isHeader(position)) {
 
-                            return new SpannedGridLayoutManager.SpanInfo(5, 1);
-                        }
-                        if (position == 15) {
-                            return new SpannedGridLayoutManager.SpanInfo(1, 2);
-                        }
-                        return new SpannedGridLayoutManager.SpanInfo(1, 1);
-                    }
-                },
-                5, // number of columns
-                1f // how big is default item
-        );
-        rvCalcButtons.setLayoutManager(manager2);
-        rvCalcButtons.setHasFixedSize(true);
-
-        View header = LayoutInflater.from(this).inflate(
-                R.layout.header, rvCalcButtons, false);
-        header.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(), "header",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
